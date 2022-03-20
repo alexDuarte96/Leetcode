df0c6bc947d2b5e75ab08ca1c8c7d7cdefeece17
Alejandro Duarte López <39140613+alexDuarte96@users.noreply.github.com>
2022-03-19 23:19:59 -0600

    Update 278. First Bad Version.swift

--- problems/278. First Bad Version.swift
+++ problems/278. First Bad Version.swift
@@ -1,9 +1,7 @@
-/**
+/*
  * The knows API is defined in the parent class VersionControl.
  *     func isBadVersion(_ version: Int) -> Bool{}
  */
-
-class Solution : VersionControl {
     func firstBadVersion(_ n: Int) -> Int {
         var left = 1
         var right = n
@@ -26,4 +24,3 @@ class Solution : VersionControl {
                 }
             }
         }
-    
